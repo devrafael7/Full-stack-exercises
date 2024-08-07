d3563cf17879bd6bcd56ada7f8095c242a670490
Rafael <rafarafa99944@gmail.com>
2024-08-07 16:18:38 -0300

    add test

---
+++ ex01/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/button-clicked', methods=['POST'])
+def button_clicked():
+    print("oi chef")
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
